92057d1b41163594fe1c6fc1a861bce56b3ec41f
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-11-18 15:05:56 +0100

    Ajout de la connexion

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
 })
+
 export class SignInComponent implements OnInit {
+    signInForm: FormGroup;
+    errorMessage: string;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.initForm();
+    }
 
+    initForm() {
+        this.signInForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]],
+        });
+    }
+
+    onSubmit() {
+        this.authService.signIn(this.signInForm.value.email, this.signInForm.value.password).then(
+            () => {
+                this.router.navigate(['']);
+            },
+            (error) => {
+                this.errorMessage = error;
+            }
+        );
+    }
 }
