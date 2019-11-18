0b850cad9f0697be5f4afce55c6502ef4ad82ace
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-11-18 14:49:41 +0100

    Ajout des redirections dans la barre de navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+    { path: 'connexion', component: SignInComponent },
+    { path: 'inscription', component: SignUpComponent }
+];
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        SignInComponent,
+        SignUpComponent
     ],
     imports: [
         BrowserModule,
