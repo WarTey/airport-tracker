465fd85fc1daf629fd5afa5ab8b87aa572624953
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-11-18 13:33:37 +0100

    Initialisation de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'AirportTracker';
+    constructor() {
+        // Web app's Firebase configuration
+        const firebaseConfig = {
+            apiKey: 'AIzaSyD83xKoJKtuuH2enK7iHSXYutt3dQizx4M',
+            authDomain: 'airport-tracker-179c5.firebaseapp.com',
+            databaseURL: 'https://airport-tracker-179c5.firebaseio.com',
+            projectId: 'airport-tracker-179c5',
+            storageBucket: 'airport-tracker-179c5.appspot.com',
+            messagingSenderId: '304653827453',
+            appId: '1:304653827453:web:9d4e76468e1cbc61112014'
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+    }
 }
