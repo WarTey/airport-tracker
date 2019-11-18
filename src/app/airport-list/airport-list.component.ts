d6c5304f12f1a7bb524e943fdc3151c162ac5173
Allan Duee <allan.duee@isen.yncrea.fr>
2019-11-18 22:29:56 +0100

    Map added

--- src/app/airport-list/airport-list.component.ts
+++ src/app/airport-list/airport-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 
 @Component({
   selector: 'app-airport-list',
@@ -9,12 +13,27 @@ import {HttpClient} from '@angular/common/http';
 export class AirportListComponent implements OnInit {
 
     airportList: any;
+    map: any;
 
     constructor(http: HttpClient) {
+        // tslint:disable-next-line:max-line-length
         http.get('https://gist.githubusercontent.com/tdreyno/4278655/raw/7b0762c09b519f40397e4c3e100b097d861f5588/airports.json').subscribe( data => { this.airportList = data; });
     }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new XYZ({
+                        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    })
+                })
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2
+            })
+        });
+    }
 }
