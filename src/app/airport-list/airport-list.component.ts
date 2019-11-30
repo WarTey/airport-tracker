91a554ee0e9e3b773f62d7f92cf0f93b9081fcd8
Allan Duee <allan.duee@isen.yncrea.fr>
2019-11-30 20:34:03 +0100

    Add leaflet module Add marker on map with leaflet Click on marker are available Popup with Airport's name, Airport's City, Airport's State and Airport's Country is display when use click on a specific Airport marker

--- src/app/airport-list/airport-list.component.ts
+++ src/app/airport-list/airport-list.component.ts
@@ -1,23 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
 import { fromLonLat } from 'ol/proj.js';
-import Vector from 'ol/source/Vector';
-import VectorL from 'ol/layer/Vector';
-import * as style from 'ol/style';
 import {AirportsService} from '../services/airports.service';
 import {Subscription} from 'rxjs';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-airport-list',
   templateUrl: './airport-list.component.html',
   styleUrls: ['./airport-list.component.css']
 })
+
 export class AirportListComponent implements OnInit {
 
     airportList: any[];
@@ -25,51 +18,37 @@ export class AirportListComponent implements OnInit {
     map: any;
     marker: any[] = [];
     airport: any;
+    airportCoordinate: any[] = [];
 
     constructor(private http: HttpClient, private airportsService: AirportsService) {
     }
 
     ngOnInit() {
+
         this.airportsSubscription = this.airportsService.airportsSubject.subscribe(
             (airports: any[]) => {
                 this.airportList = airports;
 
-                this.map = new Map({
-                    target: 'map',
-                    layers: [
-                        new TileLayer({
-                            source: new XYZ({
-                                url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                            })
-                        })
-                    ],
-                    view: new View({
-                        center: [0, 0],
-                        zoom: 2
-                    })
+                const map = L.map('map').setView([28.06847852612482, 5.668678294263798], 2);
+
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: 'map'
+                }).addTo(map);
+
+                const myIcon = L.icon({
+                    iconSize:     [20, 20],
+                    iconUrl : 'assets/airplane.png',
                 });
-                for(const element in this.airportList) {
-                    this.marker[element] = new Feature({
-                        geometry: new Point(
-                            fromLonLat([ this.airportList[element].lon, this.airportList[element].lat])
-                        ),
-                    });
 
-                    this.marker[element].setStyle(new style.Style({
-                        image: new style.Icon(({
-                            color: '#0000ff',
-                            crossOrigin: 'anonymous',
-                            src: 'assets/airplane.png',
-                        }))
-                    }));
-
-                    const vectorSource = new Vector({
-                        features: [ this.marker[element]]
-                    });
-                    const markerVectorLayer = new VectorL({
-                        source: vectorSource,
-                    });
-                    this.map.addLayer(markerVectorLayer);
+                // tslint:disable-next-line:forin
+                for (const element in this.airportList) {
+                    // tslint:disable-next-line:max-line-length
+                    this.airportCoordinate[element] =  fromLonLat([this.airportList[element].lon, this.airportList[element].lat]);
+                    // tslint:disable-next-line:max-line-length
+                    L.marker([this.airportList[element].lat, this.airportList[element].lon], {icon: myIcon}).
+                    // tslint:disable-next-line:max-line-length
+                    bindPopup('<div>' + '<strong>' + this.airportList[element].name + '</strong>' + '<p>' + this.airportList[element].city + ', '
+                        +  this.airportList[element].state +  '<br>' + this.airportList[element].country + '</p><div/>').addTo(map);
                 }
             }
         );
