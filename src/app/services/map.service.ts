1dc7e3d0124749cbe6d6f23e31246ee3085a0409
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-12-01 16:51:56 +0100

    Suppression de la fonction pour charger une map dynamique

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -4,8 +4,6 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { transform, fromLonLat } from 'ol/proj.js';
-import { createStringXY } from 'ol/coordinate.js';
-import MousePosition from 'ol/control/MousePosition';
 
 @Injectable()
 export class MapService {
@@ -32,32 +30,5 @@ export class MapService {
 
     changeMapCenter(coords: number[]) {
         this.map.getView().setCenter(transform(coords, 'EPSG:4326', 'EPSG:3857'));
-    }
-
-    loadDynamicMap(coords: number[], zoom: number) {
-        this.map = new Map({
-            target: 'map',
-            layers: [
-                new TileLayer({
-                    source: new XYZ({
-                        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                    })
-                })
-            ],
-            view: new View({
-                center: fromLonLat(coords),
-                zoom
-            })
-        });
-        this.map.on('click', (args) => {
-            console.log(transform(args.coordinate, 'EPSG:3857', 'EPSG:4326'));
-        });
-        this.map.on('pointermove', (args) => {
-            console.log(transform(args.coordinate, 'EPSG:3857', 'EPSG:4326'));
-        });
-        this.map.addControl(new MousePosition({
-            coordinateFormat: createStringXY(2),
-            projection: 'EPSG:4326',
-        }));
     }
 }
