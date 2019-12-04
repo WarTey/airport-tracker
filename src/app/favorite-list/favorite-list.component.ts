d11a72a0fa9ce479ec3820386a4257da42ca7672
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-12-05 00:13:55 +0100

    Fix du chargement des favoris

--- src/app/favorite-list/favorite-list.component.ts
+++ src/app/favorite-list/favorite-list.component.ts
@@ -6,6 +6,7 @@ import { AirportsService } from '../services/airports.service';
 import { LoadingService } from '../services/loading.service';
 import { ToastrService } from '../services/toastr.service';
 import { DataTableDirective } from 'angular-datatables';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-favorite-list',
@@ -20,16 +21,17 @@ export class FavoriteListComponent implements OnInit, OnDestroy {
     dtTrigger = new Subject();
 
     favoritesSubscription: Subscription;
-    favorites: Favorite[] = [];
     airportsSubscription: Subscription;
+    favorites: Favorite[] = [];
     airports: any = [];
-    loaded = false;
+    tableLoaded = false;
 
     constructor(private favoritesService: FavoritesService,
                 private airportsService: AirportsService,
                 private loadingService: LoadingService,
-                private toastr: ToastrService) {
-        this.loadingService.updateLoading(false);
+                private toastr: ToastrService,
+                private router: Router) {
+        this.loadingService.updateLoading(true);
     }
 
     ngOnInit() {
@@ -38,45 +40,54 @@ export class FavoriteListComponent implements OnInit, OnDestroy {
             pageLength: 10
         };
 
+        // Charge la liste complète des favoris
+        this.favoritesService.getFavorites();
         this.favoritesSubscription = this.favoritesService.favoritesSubject.subscribe(
             (favorites: Favorite[]) => {
+                // Récupère seulement les favoris qui nous appartiennent
                 this.favorites = favorites.filter(
                     favorite => this.favoritesService.checkFavorite(favorite.icao, favorite.id)
                 );
-
+                // Charge la liste complète des aéroports
                 this.airportsService.getAirports();
-                this.airportsSubscription = this.airportsService.airportsSubject.subscribe(
-                    (airports: any) => {
-                        this.airports = airports.filter(
-                            airport => {
-                                for (const element of this.favorites) {
-                                    if (element.icao === airport.icao) {
-                                        return true;
-                                    }
-                                }
+            }
+        );
+
+        this.airportsSubscription = this.airportsService.airportsSubject.subscribe(
+            (airports: any) => {
+                // Récupère les aéroports qui sont dans nos favoris
+                this.airports = airports.filter(
+                    airport => {
+                        for (const element of this.favorites) {
+                            if (element.icao === airport.icao) {
+                                return true;
                             }
-                        );
-                        if (!this.loaded) {
-                            this.dtTrigger.next();
-                            setTimeout(
-                                () => {
-                                    this.loadingService.updateLoading(true);
-                                    this.loaded = true;
-                                }, 2000
-                            );
                         }
                     }
                 );
+                // Charge le tableau au premier chargement du site
+                if (!this.tableLoaded) {
+                    this.dtTrigger.next();
+                    this.tableLoaded = true;
+                }
             }
         );
     }
 
+    onReload(icao: string) {
+        // Navigue vers une autre route (l'aéroport cliqué)
+        this.router.navigate(['details/' + icao]);
+    }
+
     onDelete(icao, name) {
+        // Supprime un aéroport des favoris
         this.favoritesService.removeFavorite(icao);
+        // Affiche un toastr d'erreur confirmant la suppression
         this.toastr.toastrError('Favoris', name + ' retiré des favoris.');
     }
 
     ngOnDestroy() {
+        // Détruit les 'Subscription'
         this.airportsSubscription.unsubscribe();
         this.favoritesSubscription.unsubscribe();
         this.dtTrigger.unsubscribe();
